1b5cec4e641685a943161733fcaaac7571027785
eunsae <jesbio@naver.com>
2025-07-16 18:55:46 +0900

    fix:SearchingView_textfield

--- Bukuksa/SearchingView.swift
+++ Bukuksa/SearchingView.swift
@@ -5,6 +5,9 @@
 //  Created by 장은새 on 7/15/25.
 //
 
+
+import Foundation
+
 import UIKit
 import SnapKit
 
@@ -16,7 +19,7 @@ class SearchingView: UIView, UITableViewDelegate, UITableViewDataSource, UITextF
     private var searchResults: [String] = []
     private let allSearchLists = ["a", "b", "c", "d"]
     //collectionViewcell이 만들어져서 밑에 연관검색어와 연결되는 영화목록이 떠야함
-   
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -37,14 +40,16 @@ class SearchingView: UIView, UITableViewDelegate, UITableViewDataSource, UITextF
         searchTextField.borderStyle = .line
         searchTextField.keyboardType = .default
         searchTextField.placeholder = "Search"
+        
         searchTextField.isHidden = true
+        
         
         searchTextField.snp.makeConstraints { make in
             make.top.equalTo(self.safeAreaLayoutGuide).offset(10)
             make.horizontalEdges.equalToSuperview().inset(20)
             make.height.equalTo(50)
         }
-
+        
         resultsTableView.delegate = self
         resultsTableView.dataSource = self
         resultsTableView.isHidden = true
@@ -62,7 +67,7 @@ class SearchingView: UIView, UITableViewDelegate, UITableViewDataSource, UITextF
         filterSearchResults(text: updatedText)
         return true
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return searchResults.count
     }
@@ -73,17 +78,18 @@ class SearchingView: UIView, UITableViewDelegate, UITableViewDataSource, UITextF
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            searchTextField.text = searchResults[indexPath.row]
-            resultsTableView.isHidden = true
-            searchResults = []
-            resultsTableView.reloadData()
-        }
-
-        func filterSearchResults(text: String) {
-            searchResults = allSearchLists.filter { $0.lowercased().hasPrefix(text.lowercased()) }
-            resultsTableView.isHidden = searchResults.isEmpty
-            resultsTableView.reloadData()
-        }
+        searchTextField.text = searchResults[indexPath.row]
+        resultsTableView.isHidden = true
+        searchResults = []
+        resultsTableView.reloadData()
+    }
+    
+    func filterSearchResults(text: String) {
+        searchResults = allSearchLists.filter { $0.lowercased().hasPrefix(text.lowercased()) }
+        resultsTableView.isHidden = searchResults.isEmpty
+        resultsTableView.reloadData()
+    }
+    
     
     public func showSearchTextField() {
         searchTextField.isHidden = false
